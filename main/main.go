11d53b41e5939e11251aec35f04bf5773d356140
hayden <358881498@qq.com>
2025-07-31 17:15:54 +0800

    任务1完成

--- main/main.go
+++ main/main.go
@@ -11,27 +11,52 @@ func main() {
 	//-------------1.1
 
 	// 声明数组
-	//var num [7]int
-	//for i := 0; i < len(num); i++ {
-	//	//数组key %2=0 赋值随机数，否则赋值上一次的随机数
-	//	if i%2 == 0 {
-	//		num[i] = getRand()
-	//	} else {
-	//		num[i] = num[i-1]
-	//	}
-	//}
-	//fmt.Println(num)
+	var num [7]int
+	for i := 0; i < len(num); i++ {
+		//数组key %2=0 赋值随机数，否则赋值上一次的随机数
+		if i%2 == 0 {
+			num[i] = getRand()
+		} else {
+			num[i] = num[i-1]
+		}
+	}
+	fmt.Println(num)
 	////调用函数--查询出现一次的数字，传入非空 整数数组，返回只出现一次的数字
-	//fmt.Println(task1.SelectOneNumber(num))
+	fmt.Println(task1.SelectOneNumber(num))
 
 	//---------------1.2
 	//调用函数--是否回文数函数，传入一个数字(false,数字转字符串回文，true,数字回文)
-	//fmt.Println(task1.IsPalindrome(121, false))
-	//fmt.Println(task1.IsPalindrome(141, true))
+	fmt.Println(task1.IsPalindrome(121, false))
+	fmt.Println(task1.IsPalindrome(141, true))
 
 	//--------------1.3
-	fmt.Println(task1.SelectBracket("("))
+	//调用检查括号函数，转入字符串返回bool
+	fmt.Println(task1.SelectBracket("()"))
+	fmt.Println(task1.SelectBracket("([])"))
+	fmt.Println(task1.SelectBracket("(][)"))
+	fmt.Println(task1.SelectBracket("({)"))
+	fmt.Println(task1.SelectBracket("({[]})"))
 
+	//--------------1.4
+	//调用检查括号函数，转入字符串返回bool
+	fmt.Println(task1.PlusOne([]int{9, 9, 9}))
+	fmt.Println(task1.PlusOne([]int{2, 9, 9}))
+	fmt.Println(task1.PlusOne([]int{0, 0, 9}))
+	fmt.Println(task1.PlusOne([]int{1, 0, 9}))
+	fmt.Println(task1.PlusOne([]int{2, 3, 4}))
+
+	//--------------1.5
+	//调用删除有序数组中的重复项函数
+	//fmt.Println(task1.RemoveDuplicates([]int{0, 0, 1, 1, 4, 7, 1, 2, 2, 3, 3, 4}))
+
+	//--------------1.6
+	//调用数组区间合并
+	fmt.Println(task1.Merge([][]int{{1, 3}, {2, 6}, {9, 15}, {19, 26}, {14, 16}}))
+	//--------------1.7
+	//调用数组两数之和
+	sum := task1.TwoSum([]int{11, 22, 33, 44, 55, 66, 77, 87, 9}, 88)
+	fmt.Println("两数之和的值是：", sum[0])
+	fmt.Println("两数之和的下标是：", sum[1])
 }
 
 // 获取随机数
--- task1/one.go
+++ task1/one.go
@@ -5,11 +5,133 @@ import (
 	"strconv"
 )
 
-func SelectBracket(param string) bool {
-	return true
+// TwoSum 两数之和
+func TwoSum(nums []int, target int) [][][]int {
+	var sum, sum1 = [][]int{}, [][]int{}
+	for i, v := range nums {
+		for ii, vv := range nums {
+			if v+vv == target {
+				var two = []int{v, vv}
+				var two1 = []int{i, ii}
+				sum = append(sum, two)
+				sum1 = append(sum1, two1)
+			}
+		}
+	}
+	return [][][]int{sum, sum1}
 }
 
-// 查询只出现一次的数字
+// Merge 区间合并
+func Merge(arr [][]int) [][]int {
+	n := len(arr)
+	//数组中第一个值按升序排序
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j][0] > arr[j+1][0] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	var merged = [][]int{arr[0]}
+	for _, v := range arr {
+		// 没有重叠，直接添加到结果中
+		if v[0] > merged[len(merged)-1][1] {
+			merged = append(merged, v)
+		} else {
+			// 有重叠，合并区间
+			var n int
+			if merged[len(merged)-1][1] > v[1] {
+				n = merged[len(merged)-1][1]
+			} else {
+				n = v[1]
+			}
+			merged[len(merged)-1][1] = n
+		}
+	}
+	return merged
+}
+
+// RemoveDuplicates 删除有序数组中的重复项
+func RemoveDuplicates(nums []int) []int {
+	var arr = []int{nums[0]}
+	var b int
+outerLoop:
+	for i := 0; i < len(nums); i++ {
+		for _, v := range arr {
+			b = nums[i]
+			if v == nums[i] {
+				b = 0
+				continue outerLoop
+			}
+		}
+		if b > 0 {
+			arr = append(arr, b)
+			b = 0
+		}
+	}
+	return arr
+}
+
+// PlusOne 数组+1
+func PlusOne(param []int) []int {
+	l := len(param)
+	if l == 0 {
+		return []int{1}
+	}
+	var in = make(map[int]int)
+	for i := l; i > 0; i-- {
+		ii := i - 1
+		if param[ii] >= 9 {
+			if i == l {
+				param[ii] = 0
+			} else {
+				param[ii] = -1
+			}
+			in[i-2]++
+		} else {
+			if i == l {
+				param[i-1]++
+			}
+		}
+		if in[i-1] > 0 {
+			param[i-1]++
+			in[i-1] = 0
+		}
+		if value, ok := in[-1]; ok {
+			param = append(param[:0], append([]int{value}, param[0:]...)...)
+		}
+	}
+	return param
+}
+
+// SelectBracket 有效的括号
+func SelectBracket(param string) bool {
+	stack := make([]rune, 0)
+	mapping := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
+	for _, char := range param {
+		switch char {
+		case '(', '[', '{':
+			stack = append(stack, char)
+		case ')', ']', '}':
+			if len(stack) == 0 {
+				return false
+			}
+			top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if mapping[char] != top {
+				return false
+			}
+		}
+	}
+
+	return len(stack) == 0
+}
+
+// SelectOneNumber 查询只出现一次的数字
 func SelectOneNumber(param [7]int) int {
 	var result int
 	for _, e := range param {
@@ -18,7 +140,7 @@ func SelectOneNumber(param [7]int) int {
 	return result
 }
 
-// 回文数
+// IsPalindrome 回文数
 func IsPalindrome(param int, is bool) bool {
 	if is {
 		//数字回文
